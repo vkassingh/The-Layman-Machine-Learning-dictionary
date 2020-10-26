da5f85904a7181792d61528c261c2876bca567c4
AniruthA <60116733+AniruthA@users.noreply.github.com>
2020-10-26 16:12:47 -0500

    Update QLearningAgent.py

--- examples/QLearningAgent.py
+++ examples/QLearningAgent.py
@@ -1,61 +1,76 @@
-from environment import State, getUser, act
-import numpy as np
+from copy import deepcopy
 import random
 
-random.seed(42)
+class State:
+  BOTTOM_LESS_PIT = "O"
+  GOAL = "$"
+  EMPTY = "*"
+  USER = "U"
 
-N_STATES = 4
-N_EPISODES = 20000
-MIN_ALPHA = 0.2
+  UP, DOWN, LEFT, RIGHT = 0,1,2,3
 
-alphas = np.linspace(1.0, MIN_ALPHA, N_EPISODES)
-gamma = 1.0
-eps = 0.4
+  ACTIONS = [ UP, DOWN, LEFT, RIGHT ]
 
-print(State())
+  def __init__(self,grid=False):
+    self.grid = [
+      [self.GOAL,self.EMPTY,self.EMPTY,self.EMPTY,self.EMPTY],
+      [self.EMPTY,self.BOTTOM_LESS_PIT,  self.EMPTY,self.EMPTY, self.EMPTY],
+      [self.EMPTY,self.EMPTY,self.BOTTOM_LESS_PIT,self.EMPTY,self.USER],
+    ]
+    if grid:
+      self.grid = grid
+    self.userPos = [2,2]
 
-qTable = {}
+  def __repr__(self):
+    string = ""
+    string += " "+"- - "*len(self.grid[0]) + "\n"      
+    for i in self.grid:
+      string += "| "
+      string += " | ".join(i) + " |\n"
+    string += " "+"- - "*len(self.grid[0])      
+    return string
 
-def q(state, action=None): 
-  if state not in qTable:
-    qTable[state] = np.zeros(len(state.ACTIONS))  
-  if action is None:
-    return qTable[state]
-    
-  return qTable[state][action]
+  def __hash__(self):
+    return hash(str(self.grid) + str(self.userPos))
 
-def choose_action(state):
-  if random.uniform(0, 1) < eps:
-    return random.choice(state.ACTIONS) 
-  else:
-    return np.argmax(q(state))
+def act(state,action):
+  def new_pos(state,action):
+    pos = [state.userPos[0],state.userPos[1]]
+    if action == 0:
+      pos[1] = max(0,pos[1]-1)
+    elif action == 1:
+      pos[1] = min(len(state.grid)-1,pos[1]+1)
+    elif action == 2:
+      pos[0] = max(0,pos[0]-1)
+    elif action == 3:
+      pos[0] = min(len(state.grid[0])-1,pos[0]+1)
+    return pos
+  
+  isDone = False
+  reward = 0
 
-for e in range(N_EPISODES):
-  state = State()
-  total_reward = 0
-  alpha = alphas[e]
-  done = False
-    
-  while not done:
-    action = choose_action(state)
-    next_state, done, reward = act(state, action)
-    next_state = State(next_state)
-    total_reward += reward
-        
-    q(state)[action] = q(state, action) + alpha * (reward + gamma *  np.max(q(next_state)) - q(state, action))
-    state.grid = next_state.grid
-    state.userPos = getUser(state)
+  if action in (0,1,2,3):
+    px,py = new_pos(state,action)
+    gridItem = state.grid[py][px]
+    newGrid = deepcopy(state.grid)
 
-  print(f"Episode {e+1} --> {total_reward}")
+    if gridItem == state.BOTTOM_LESS_PIT:
+      reward = -10
+      isDone = True
+    elif gridItem == state.EMPTY or gridItem == state.USER:
+      reward = -.01
+      oldx,oldy = state.userPos
+      newGrid[oldy][oldx] = state.EMPTY
+      newGrid[py][px] = state.USER
+    elif gridItem == state.GOAL:
+      isDone = True
+      reward = 100
+    return newGrid, isDone, reward
 
-while not done:
-  action = np.argmax(q(state))
-  next_state, done, reward = act(state, action)
-  next_state = State(next_state)
-  total_reward += reward
-        
-  q(state)[action] = q(state, action) + alpha * (reward + gamma *  np.max(q(next_state)) - q(state, action))
-  state.grid = next_state.grid
-  state.userPos = getUser(state)
 
-print("Score : "+str(total_reward))
+def getUser(s):
+  for ri in range(0,len(s.grid)):
+    if s.USER in s.grid[ri]:
+      for i in range(0,len(s.grid[0])):
+        if s.grid[ri][i] == s.USER:
+          return i, ri
